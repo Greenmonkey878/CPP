f479dd24e074a1f22d4bc39abc1d5c30be16b8da
Greenmonkey878 <96891123+Greenmonkey878@users.noreply.github.com>
2023-04-12 16:58:39 -0500

    Add files via upload

---
+++ Euros/Euros.mk
@@ -0,0 +1,109 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=Euros
+ConfigurationName      :=Debug
+WorkspaceConfiguration :=Debug
+WorkspacePath          :=D:/CodingStuff/C++Projects/C++
+ProjectPath            :=D:/CodingStuff/C++Projects/C++/Euros
+IntermediateDirectory  :=$(ConfigurationName)
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=Archangel
+Date                   :=12/04/2023
+CodeLitePath           :=D:/CodeLite
+LinkerName             :=E:/mingw64/bin/g++.exe
+SharedObjectLinkerName :=E:/mingw64/bin/g++.exe -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputDirectory        :=$(IntermediateDirectory)
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName).exe
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="Euros.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=D:/CodeLite/mkdir.exe -p
+RcCmpOptions           := 
+RcCompilerName         :=E:/mingw64/bin/windres.exe
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overridden using an environment variable
+##
+AR       := E:/mingw64/bin/ar.exe -r
+CXX      := E:/mingw64/bin/g++.exe
+CC       := E:/mingw64/bin/gcc.exe
+CXXFLAGS :=  -gdwarf-2 -O0 -Wall $(Preprocessors)
+CFLAGS   :=  -gdwarf-2 -O0 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := E:/mingw64/bin/as.exe
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=D:\CodeLite
+Objects0=$(IntermediateDirectory)/main.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@$(MakeDirCommand) "$(ConfigurationName)"
+
+
+$(IntermediateDirectory)/.d:
+	@$(MakeDirCommand) "$(ConfigurationName)"
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/CodingStuff/C++Projects/C++/Euros/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
+
+##
+## Clean
+##
+clean:
+	$(RM) -r $(ConfigurationName)/
+
+
