7e3357ff84b2bf17cb75f20ab5b1d251baa9bb6c
Greenmonkey878 <96891123+Greenmonkey878@users.noreply.github.com>
2023-04-15 12:23:38 -0500

    Add files via upload

---
+++ Ringcords/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    //double cord1{},cord2,{},cord3{},cord4{},cord5{},cord6{},cord7{},cord8{};
+    
+    
+    cout << "From machine enter Value of X: " <<endl;
+    cout << "From machine enter Value of Y: " <<endl;
+    cout << "From part enter value to increase or decrease: " <<endl;
+    
+    
+    
+    cout << "Enter which hole section you wish to move 1,2,3,4 for moving X and Y, or 5 for adding just X, 6 for adding just Y, 7 for minus X, 8 for minus Y: " << endl;
+    
+    
+    
+    
+    
+    
+    
+    
+
+    return 0;
+}
