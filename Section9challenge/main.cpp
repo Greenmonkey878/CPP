ae12b78e1ff28df97b92bef27ab6b694ef9c3332
Greenmonkey878 <96891123+Greenmonkey878@users.noreply.github.com>
2023-05-02 16:32:07 -0500

    Add files via upload

--- Section9challenge/main.cpp
+++ Section9challenge/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <iomanip>
 
 using namespace std;
 
@@ -21,8 +20,6 @@ int main(){
         cout << "================================" << endl;
         cout << "\nEnter your selection: ";
         cin >> selection;
-
-        
         
         if(selection == 'p' || selection == 'P'){
             if(numbers.size() == 0)
@@ -30,10 +27,9 @@ int main(){
             else{
                 cout << "[ ";
                 for(auto num: numbers)
-                    cout << num << ",";
+                    cout << num << " ";
                 cout << "]";
             }
-            
         }
         else if(selection == 'a' || selection == 'A'){
             int numbers_added{};
@@ -42,30 +38,46 @@ int main(){
             numbers.push_back(numbers_added);
             cout << numbers_added << " added" << endl;
         }
-            
         else if(selection == 'm' || selection == 'M'){
-            
+            if(numbers.size() == 0)
+                cout << "Unable to calculate mean - no data" << endl;
+            else{
+                int total{};
+                for(auto num: numbers)
+                    total += num;
+                cout << "The mean is: " << static_cast<double>(total)/numbers.size() << endl;
+            }
         }
-            
         else if(selection == 's' || selection == 'S'){
-            
+            if(numbers.size() == 0)
+                cout << "Unable to determine the smallest - list is empty" << endl;
+            else{
+                int smallest = numbers.at(0);
+                for(auto num:numbers)
+                    if(num < smallest)
+                        smallest = num;
+                cout << "The smallest number is: " << smallest << endl;
+            }
         }
         else if(selection == 'l' || selection == 'L'){
-            
+            if(numbers.size() == 0)
+                cout << "Unable to determine the largest - list is empty" << endl;
+            else{
+                int largest = numbers.at(0);
+                for(auto num:numbers)
+                    if(num > largest)
+                        largest = num;
+                cout << "The largest number is: " << largest << endl;
+            }            
         }
-        else
-            cout << "Invalid selection" << endl;
-            
+        else if(selection == 'q' || selection == 'Q'){
+            cout << "Goodbye" << endl;
+        }
+        else{
+            cout << "Unkown selection" << endl;
+        }
             
     }while (selection != 'q' && selection != 'Q');
         
-        
-    
-    
-    
-    
-    
-    
-    
     return 0;
 }
